92c083c48f58254c60bf832df3e6ad41bc9fece3
ketkidn <47437361+ketkidn@users.noreply.github.com>
2024-02-29 16:51:22 +0530

    Update jira_config.py

--- jira_config.py
+++ jira_config.py
@@ -1,4 +1,4 @@
 JIRA_URL= 'https://allvuesystems-sandbox-200.atlassian.net/'  # Replace with your Jira URL
-JIRA_USER = 'kshastri@allvuesystems.com'             # Replace with your Jira email
-JIRA_TOKEN = 'ATATT3xFfGF0hulJFM5muAPf00pLVJOqyFJSnxMiaq_XF724pEQtSZjc9HUPydOz4FsiPf23e1-HGAofsLTxQEicn4Uzcnxchat6W0fM50lb-hyxPcskHGbLorfs61q3Hnd4eWN4E2Qm3XVC9LVQMak1jDqwOxArQg8fjv24oGAp9Qp_kRsic2U=D658FB5C'                     # Replace with your Jira API token
-OpenAPI_KEY="5d53e64922f14315a77bc7619c362414"
+JIRA_USER = ' '             # Replace with your Jira email
+JIRA_TOKEN = '  '                     # Replace with your Jira API token
+OpenAPI_KEY=" " #put your openAI key
